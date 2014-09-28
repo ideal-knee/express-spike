c99888e556dbb88ca358ad3452a73500353d65ff
Dan Kee <dan@dankee.com>
2014-09-27 23:39:24 -0500

    Add lein-cljsbuild.

--- project.clj
+++ project.clj
@@ -2,5 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+            :url "http://www.eclipse.org/legal/epl-v10.html" }
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2356"] ]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :cljsbuild {:builds []} )
+
