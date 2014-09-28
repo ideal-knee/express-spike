cd4ae7871f4aa386ceb89992f4debdbce9398705
Dan Kee <dan@dankee.com>
2014-09-27 23:11:42 -0500

    lein new express-spike

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to express-spike
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/express_spike/core_test.clj
@@ -0,0 +1,7 @@
+(ns express-spike.core-test
+  (:require [clojure.test :refer :all]
+            [express-spike.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
